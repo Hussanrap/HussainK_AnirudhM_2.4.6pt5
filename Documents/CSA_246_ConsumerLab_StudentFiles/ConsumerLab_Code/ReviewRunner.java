c4f13858d385b19c26c30744b4e13c925e4d6bdd
Hussain Kazi <kazi0561@mydusd.org>
2024-10-21 13:35:31 -0700

    First pull

---
+++ Documents/CSA_246_ConsumerLab_StudentFiles/ConsumerLab_Code/ReviewRunner.java
@@ -0,0 +1,9 @@
+class ReviewRunner {
+  public static void main(String[] args) 
+  {
+    /* your code here, for example: */
+    System.out.println(Review.sentimentVal("terrible"));
+    System.out.println(Review.sentimentVal("great"));
+  }
+ 
+}
